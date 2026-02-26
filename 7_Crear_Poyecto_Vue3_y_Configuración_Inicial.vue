839c74c7924c53df37c58d9367bedb53ba1bba8c
czelayaITCHA <83366939+czelayaITCHA@users.noreply.github.com>
2026-02-26 10:18:17 -0600

    Create 7_Crear_Poyecto_Vue3_y_Configuración_Inicial.vue

---
+++ 7_Crear_Poyecto_Vue3_y_Configuración_Inicial.vue
@@ -0,0 +1,4 @@
+# Creación de proyecto Vue 3 tipo SPA
+## I. Introducción
+## 1. Crear proyecto Vue 3
+**¿Que es Vue 3? ** es la última versión del framework JavaScript progresivo para construir interfaces de usuario, lanzado en 2020. Destaca por ser más rápido, pequeño y eficiente que Vue 2, introduciendo la Composition API para organizar código complejo, un mejor soporte para TypeScript y un sistema de reactividad basado en Proxies ES6. 
